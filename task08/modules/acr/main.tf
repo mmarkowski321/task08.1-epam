72a90296daf256a8acf8aea7836c91c031041dc9
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-14 17:11:40 +0200

    Woerking lab8.1-3

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -6,3 +6,25 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
   tags                = var.tags
 }
+
+resource "azurerm_container_registry_task" "build" {
+  name                  = "build-task"
+  container_registry_id = azurerm_container_registry.acr.id
+  enabled               = true
+  timeout_in_seconds    = 3600
+  is_system_task        = false
+  tags                  = var.tags
+
+  platform {
+    os = "Linux"
+  }
+
+  docker_step {
+    context_path         = "https://github.com/mmarkowski321/task08.1-epam.git"
+    dockerfile_path      = "application/Dockerfile"
+    image_names          = [var.image_name]
+    push_enabled         = true
+    cache_enabled        = true
+    context_access_token = var.git_pat
+  }
+}
--- task08/modules/acr/variables.tf
+++ task08/modules/acr/variables.tf
@@ -1,10 +1,28 @@
-variable "name" { type = string }
-variable "location" { type = string }
-variable "resource_group" { type = string }
-variable "sku" { type = string }
-variable "image_name" { type = string }
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
 variable "git_pat" {
   type      = string
   sensitive = true
 }
-variable "tags" { type = map(string) }
+
+variable "tags" {
+  type = map(string)
+}
