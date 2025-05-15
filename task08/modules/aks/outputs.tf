74f272e76ead9c2b137c2e055a44973fbfd86181
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-15 23:44:52 +0200

    lab08-4

--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -1,9 +1,3 @@
-
-output "kube_config_raw" {
-  description = "Raw kube config for kubectl provider"
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
-
 output "kubelet_identity" {
   description = "The kubelet identity block with client_id and object_id"
   value       = azurerm_kubernetes_cluster.aks.kubelet_identity
