df39d898f0ddbb9a883c08f614368fc695e91fdc
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-15 23:41:52 +0200

    lab08-3

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -5,11 +5,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    name         = var.node_pool_name
-    node_count   = var.node_count
-    vm_size      = var.node_vm_size
-    os_disk_size_gb     = 30
-    os_disk_type = "Ephemeral"
+    name            = var.node_pool_name
+    node_count      = var.node_count
+    vm_size         = var.node_vm_size
+    os_disk_size_gb = 30
+    os_disk_type    = "Ephemeral"
   }
 
   identity {
