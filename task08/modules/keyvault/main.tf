f839c68b7146bc8e08fd2f0611305d0aacf965e4
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-16 00:57:57 +0200

    lab08-8

--- task08/modules/keyvault/main.tf
+++ task08/modules/keyvault/main.tf
@@ -17,9 +17,6 @@ resource "azurerm_key_vault_access_policy" "current_user" {
   secret_permissions = [
     "Get", "List", "Set", "Delete", "Purge", "Recover", "Backup", "Restore"
   ]
-  key_permissions = [
-    "Get", "List", "Set", "Delete", "Purge", "Recover", "Backup", "Restore"
-  ]
 }
 
 resource "azurerm_key_vault_access_policy" "aks" {
@@ -29,9 +26,6 @@ resource "azurerm_key_vault_access_policy" "aks" {
 
   secret_permissions = [
     "Get", "List"
-  ]
-  key_permissions = [
-    "Get", "List", "Encrypt", "Decrypt"
   ]
 }
 
