97c83495bfd3072a984d1dc13c93fbbe4d0efa74
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-05-16 01:37:07 +0200

    lab08-0

--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -7,3 +7,22 @@ output "kube_config_raw" {
   description = "Raw kube config for kubectl provider"
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
+
+output "host" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate
+  sensitive = true
+}
